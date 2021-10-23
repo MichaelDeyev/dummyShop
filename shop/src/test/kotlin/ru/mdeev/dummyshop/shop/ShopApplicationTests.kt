6318e2b3136157b3ee5fe4f942ba043e7b16804f
Mikhail Deyev <mdeev@neoflex.ru>
2021-10-23 18:29:26 +0300

    initial commit

---
+++ shop/src/test/kotlin/ru/mdeev/dummyshop/shop/ShopApplicationTests.kt
@@ -0,0 +1,12 @@
+package ru.mdeev.dummyshop.shop
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
